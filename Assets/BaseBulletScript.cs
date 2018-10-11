239455933fba68098c8e554d7aade04dc92b828b
frnce <francetag32@gmail.com>
2018-10-11 21:19:00 +0800

    Initial Shooting and knockback is implemented

---
+++ Assets/BaseBulletScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBulletScript : MonoBehaviour
+{
+    public float speed;
+    public bool onDetonate = false;
+    Vector3 direction;
+    Rigidbody2D rb2d;
+
+    public void Setup(Vector3 _direction)
+    {
+        direction = _direction;
+    }
+    // Use this for initialization
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb2d.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        Destroy(gameObject, 1f);
+        //TODO look for way for the player to knockback
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //TODO Consider for other Player Controllers
+    public Transform weapon;
+    public bool autoFire = false;
+    public Transform spawnPoint;
+    public GameObject bullet;
+
+    public float knockbackRate;
+
+    Rigidbody2D rb2d;
+
+    float horizontalInput = 0;
+    float verticalInput = 0;
+    bool fireInput;
+
+    Vector3 shootVector;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        GetInput();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+        if (!autoFire)
+        {
+            Aim();
+            Shoot();
+        }
+        else
+        {
+            //here you put without Aiming , it will fire as you press a direction keys
+        }
+	}
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        fireInput = Input.GetKeyDown(KeyCode.K);
+    }
+
+    void Aim()
+    {
+        //gets the direction the input
+        Vector3 aimVector = new Vector3(horizontalInput, verticalInput, 0);
+        //Check if the user presses an input
+        if(aimVector.x != 0 || aimVector.y != 0)
+        {
+            shootVector = aimVector;
+            //Calculate the Angle of the weapon and brings the direction to life.
+            float gunAngle = Mathf.Atan2(aimVector.y, aimVector.x) * Mathf.Rad2Deg;
+            weapon.rotation = Quaternion.Euler(new Vector3(0, 0, gunAngle));
+            Debug.Log(aimVector);
+        }
+    }
+    void Shoot()
+    {
+        if (fireInput)
+        {
+            Vector3 spawnPos = spawnPoint.position;
+            Quaternion spawnRot = Quaternion.identity;
+
+            BaseBulletScript bul = Instantiate(bullet, spawnPos, spawnRot).GetComponent<BaseBulletScript>();
+            bul.Setup(shootVector);
+
+            KnockBack(shootVector);
+        }
+    }
+
+    void KnockBack(Vector3 direction)
+    {
+        rb2d.AddForce(direction * -knockbackRate, ForceMode2D.Impulse);
+    }
+}
